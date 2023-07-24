d205c83167d28bedd432c27b8be6efa66e070a9e
nkechit-j <nkechi.taribo@gmail.com>
2023-07-25 00:00:20 +0100

    registeration

--- api/auth/account.php
+++ api/auth/account.php
@@ -7,7 +7,8 @@ class Register{
      private $message = [];
 
     public function __construct(){
-
+            // execute the function 
+            $this->createUser();
     }
 
     public function createUser(){
@@ -22,13 +23,28 @@ class Register{
              
            // double check to see if the two password matches
            if($password1 === $password2){
+                //    Hass the password 
+                $password = md5($password1);
+                //  Store the user information to the database
+                $sql = "INSERT INTO `users`(`st_email`, `st_username`,`st_pass`, `createdAt`) VALUES( '$username','$student__email','$password',NOW() )";
+                $query = $this->con()->query($sql);
+                // check if the query succeeded 
+                if($query){
+                    $this->message['message'] = "success";
+                }else{
+                    $this->message['message'] = "Error Occured"; 
+                }
 
            }else{
-            
+             $this->message['message'] = "Password Mismatch";
            }
 
+       }else{
+          $this->message['message'] = "All fields are required";
        }
 
+    //  finalyy send a json response
+     json_encode($this->message);
 
     }
     
